29f82326260119dd8055de39554fdbf85a77b428
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-08-21 15:58:30 -0500

    exaBinSort: Minor changes to API

--- src/exasort-binsort.h
+++ src/exasort-binsort.h
@@ -1,3 +1,4 @@
+#include "exa.h"
 #include "exasort-field.h"
 
 #include <math.h>
@@ -5,8 +6,11 @@
 #include <time.h>
 #include <limits.h>
 
-#define exaBinSort(T,array,S,field,proc,comm,buf) \
+#define exaSortBinSort(T,array,S,field,proc,comm,buf) \
   sarray_sort(T,array,exaArrayGetSize(array),field,exaTypeGetType(S),buf); /* local sort */ \
   exaArraySetProc(T,array,S,field,proc,comm); /* Set destination bin */ \
   exaArrayTransfer(T,array,proc,comm->cr); /* Transfer to destination processor */ \
   sarray_sort(T,array,exaArrayGetSize(array),field,exaTypeGetType(S),buf); /* locally sort again */
+
+#define exaBinSort(h,T,array,S,field,proc) \
+  exaSortBinSort(T,array,S,field,proc,exaGetComm(h),h->buf);
