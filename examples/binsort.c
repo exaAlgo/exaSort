5a2b38b64494c3e1faa68ee7ebbfc77da4314c0c
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-09-02 17:34:19 -0500

    binsort: Update example

--- examples/binsort.c
+++ examples/binsort.c
@@ -16,28 +16,19 @@ int main() {
   exaInit(&h,MPI_COMM_WORLD);
 
   exaArray arr;
-  exaArrayCreate(kvPair,arr,5);
+  exaArrayCreate(kvPair,arr,exaRank(h)+5);
   kvPair *data=exaArrayPointer(kvPair,arr);
 
   int i,j;
-  printf("Before sorting -- Array size: %d\n",exaArraySize(arr));
-  for(j=0; j<exaSize(h); j++) {
-    exaBarrier(h);
-    if(j==exaRank(h)) {
-      for(i=0; i<exaArraySize(arr); i++){
-        data[i].key=i;
-        data[i].value=5*exaSize(h)-(5*exaRank(h)+i);
-        printf("rank: %d %d key=" exaIntFormat ", value=" exaScalarFormat "\n",exaRank(h),i,\
-            data[i].key,data[i].value);
-      }
-    }
-    fflush(stdout);
+  for(i=0; i<exaArraySize(arr); i++){
+    data[i].key=i;
+    data[i].value=-(exaRank(h)+ i*(1./exaArraySize(arr)));
   }
 
   exaBinSort(h,kvPair,arr,exaScalar,value,proc);
 
   data=exaArrayPointer(kvPair,arr);
-  printf("After sorting -- Array size: %d\n",exaArraySize(arr));
+  if(exaRank(h)==0) printf("After sorting\n");
   for(j=0; j<exaSize(h); j++) {
     exaBarrier(h);
     if(j==exaRank(h)) {
@@ -45,8 +36,8 @@ int main() {
         printf("rank: %d %d key=" exaIntFormat ", value=" exaScalarFormat "\n",exaRank(h),i,\
             data[i].key,data[i].value);
       }
+      fflush(stdout);
     }
-    fflush(stdout);
   }
 
   exaArrayFree(arr);
