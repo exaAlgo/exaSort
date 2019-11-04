525437429f3b9449d9f42a467f8452903eef5e82
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-11-04 02:00:54 -0600

    Minor

--- examples/binsort.c
+++ examples/binsort.c
@@ -18,25 +18,25 @@ int main() {
   exaArray arr;
   size_t size=exaRank(h)+5;
   exaArrayInit(&arr,sizeof(kvPair),size);
-  exaArraySetSize(arr,size);
 
-  kvPair *data=exaArrayGetPointer(arr);
+  kvPair data;
   int i,j;
   for(i=0; i<size; i++){
-    data[i].key=i;
-    data[i].value=-(exaRank(h)+ i*(1./exaArrayGetSize(arr)));
+    data.key=i;
+    data.value=-(exaRank(h)+ i*(1./exaArrayGetMaxSize(arr)));
+    exaArrayAppend(arr,&data);
   }
 
   exaBinSort(h,kvPair,arr,exaScalar,value,proc);
 
-  data=exaArrayGetPointer(arr);
+  kvPair *arrayData=exaArrayGetPointer(arr);
   if(exaRank(h)==0) printf("After sorting\n");
   for(j=0; j<exaSize(h); j++) {
     exaBarrier(h);
     if(j==exaRank(h)) {
       for(i=0; i<exaArrayGetSize(arr); i++){
         printf("rank: %d %d key=" exaIntFormat ", value=" exaScalarFormat "\n",exaRank(h),i,\
-            data[i].key,data[i].value);
+            arrayData[i].key,arrayData[i].value);
       }
       fflush(stdout);
     }
--- src/exasort.h
+++ src/exasort.h
@@ -13,8 +13,8 @@
 #define exaSortLocalSort2(h,T,array_,S1,field1,S2,field2) do{ \
   exaInt size=exaArrayGetSize(array_); \
   T *ptr=exaArrayGetPointer(array_); \
-  sarray_sort2(T,ptr,(exaUInt)size,field1,exaTypeGetGSSortType(S1), \
-                                   field2,exaTypeGetGSSortType(S2),&(h->buf)); \
+  sarray_sort_2(T,ptr,(exaUInt)size,field1,exaTypeGetGSSortType(S1), \
+                                    field2,exaTypeGetGSSortType(S2),&(h->buf)); \
 }while(0)
 
 #define exaSortLocalSort3(h,T,array_,S1,field1,S2,field2,S3,field3) do{ \
